85d6733428cb7a63869d2ea9195b7810dfd6712e
Ebrahim Olied <99254936+Darkh4x0r@users.noreply.github.com>
2022-02-14 10:28:43 -0800

    Update 0steg.sh

--- 0steg.sh
+++ 0steg.sh
@@ -12,9 +12,6 @@ echo " ███████████                                 ██�
                                                                           ░░██████ 
                                                                            ░░░░░░  "
 
-
-success=echo -e "\e[31mInstalling Ssuccessful\e[0m""\n"
- 
 echo -e "\e[31mMade by D4rkhax0r\e[0m""\n"
 
 echo " This tool is for CTF Steganography "
@@ -24,17 +21,17 @@ echo -e " Please be sure to be root before running this tool  "
 echo -e "Editing your sources.list "
 
 
-echo "deb http://http.kali.org/kali kali-rolling main contrib non-free" | sudo tee /etc/apt/sources.list
-echo "deb http://http.kali.org/kali sana main non-free contrib" | sudo tee /etc/apt/sources.list
-echo "deb http://security.kali.org/kali-security sana/updates main contrib non-free" | sudo tee /etc/apt/sources.list
-echo "deb http://old.kali.org/kali moto main non-free contrib" | sudo tee /etc/apt/sources.list
-echo "deb http://ap-northeast-2.ec2.archive.ubuntu.com/ubuntu/ bionic-updates main restricted" | sudo tee /etc/apt/sources.list
-echo "deb http://ap-northeast-2.ec2.archive.ubuntu.com/ubuntu/ bionic universe" | sudo tee /etc/apt/sources.list
-echo "deb http://ap-northeast-2.ec2.archive.ubuntu.com/ubuntu/ bionic-updates universe" | sudo tee /etc/apt/sources.list
-echo "deb http://ap-northeast-2.ec2.archive.ubuntu.com/ubuntu/ bionic multiverse" | sudo tee /etc/apt/sources.list
-echo "deb http://ap-northeast-2.ec2.archive.ubuntu.com/ubuntu/ bionic-updates multiverse" | sudo tee /etc/apt/sources.list
-echo "deb http://ap-northeast-2.ec2.archive.ubuntu.com/ubuntu/ bionic-backports main restricted universe multiv" | sudo tee /etc/apt/sources.list
-echo "deb http://security.ubuntu.com/ubuntu/ bionic-security universe"| sudo tee /etc/apt/sources.list
+echo "deb http://http.kali.org/kali kali-rolling main contrib non-free" | sudo tee -a /etc/apt/sources.list
+echo "deb http://http.kali.org/kali sana main non-free contrib" | sudo tee -a /etc/apt/sources.list
+echo "deb http://security.kali.org/kali-security sana/updates main contrib non-free" | sudo tee -a /etc/apt/sources.list
+echo "deb http://old.kali.org/kali moto main non-free contrib" | sudo tee -a /etc/apt/sources.list
+echo "deb http://ap-northeast-2.ec2.archive.ubuntu.com/ubuntu/ bionic-updates main restricted" | sudo tee -a /etc/apt/sources.list
+echo "deb http://ap-northeast-2.ec2.archive.ubuntu.com/ubuntu/ bionic universe" | sudo tee -a /etc/apt/sources.list
+echo "deb http://ap-northeast-2.ec2.archive.ubuntu.com/ubuntu/ bionic-updates universe" | sudo tee -a /etc/apt/sources.list
+echo "deb http://ap-northeast-2.ec2.archive.ubuntu.com/ubuntu/ bionic multiverse" | sudo tee -a /etc/apt/sources.list
+echo "deb http://ap-northeast-2.ec2.archive.ubuntu.com/ubuntu/ bionic-updates multiverse" | sudo tee -a /etc/apt/sources.list
+echo "deb http://ap-northeast-2.ec2.archive.ubuntu.com/ubuntu/ bionic-backports main restricted universe multiv" | sudo tee -a /etc/apt/sources.list
+echo "deb http://security.ubuntu.com/ubuntu/ bionic-security universe"| sudo tee -a /etc/apt/sources.list
 
 
 sudo apt-get update  
@@ -84,23 +81,17 @@ echo -e "\e[31mInstalling Stegseek\e[0m""\n"
 
 wget "https://github.com/RickdeJager/stegseek/releases/download/v0.6/stegseek_0.6-1.deb" -O stegseek.deb
 chmod +x stegseek.deb
-sudo apt install ./stegseek.deb -y 
+ 
+ sudo apt install ./stegseek.deb -y 
 
-echo -e "\e[31mDownloading rockyou.txt\e[0m""\n"
+#echo -e "\e[31mDownloading rockyou.txt\e[0m""\n"
 
-wget "https://raw.githubusercontent.com/danielmiessler/SecLists/master/Passwords/Leaked-Databases/rockyou-75.txt" -O rockyou.txt
-
-
+#wget "https://raw.githubusercontent.com/danielmiessler/SecLists/master/Passwords/Leaked-Databases/rockyou-75.txt" -O rockyou.txt
 
 echo -e "\e[31mInstalling Audacity\e[0m""\n"
 
 sudo apt-get install audacity
 
 
-
-echo -e "\e[31mDownloading tools commands\e[0m""\n"
-
-wget " https://raw.githubusercontent.com/Darkh4x0r/0steg/main/Commands.txt"
-
-$success
+echo -e "\e[31mSuccess \e[0m""\n"
 
